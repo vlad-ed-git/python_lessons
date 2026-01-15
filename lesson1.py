87f6da07c213365d19885d16b59b1bd2fcf21040
Vladimir Peter <vlad.edna@gmail.com>
2026-01-15 14:25:29 -0800

    Implement run_server function in lesson1.py
    Added a function to run the HTTP server.

--- lesson1.py
+++ lesson1.py
@@ -6,3 +6,11 @@ Serves data from an in-memory array
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 from urllib.parse import urlparse, parse_qs
+
+
+
+def run_server(port=8000):
+    """TODO Start the server"""
+
+if __name__ == '__main__':
+    run_server()
